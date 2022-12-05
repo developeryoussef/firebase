f8a53bc2d5742c3944b10855316048f64f8ca3e8
developeryoussef <futuredeveloperysf1102@gmail.com>
2022-12-05 21:43:57 +0200

    i

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
+// ignore_for_file: use_build_context_synchronously, unused_local_variable, avoid_print, avoid_function_literals_in_foreach_calls
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.android,
   );
-  runApp(FirstFirebaseP());
+  runApp(const FirstFirebaseP());
 }
 
 class FirstFirebaseP extends StatefulWidget {
@@ -19,116 +22,37 @@ class FirstFirebaseP extends StatefulWidget {
 }
 
 class _FirstFirebasePState extends State<FirstFirebaseP> {
-  var username, password, email;
-  GlobalKey<FormState> formState = new GlobalKey<FormState>();
+  List users = [];
 
-  signIn() async {
-    var formdata = formState.currentState;
+  CollectionReference useref = FirebaseFirestore.instance.collection('users');
 
-    if (formdata!.validate()) {
-      debugPrint('Valid');
-      formdata.save();
-      try {
-        final credential =
-            await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: email,
-          password: password,
-        );
-      } on FirebaseAuthException catch (e) {
-        if (e.code == 'weak-password') {
-          debugPrint('The password provided is too weak.');
-        } else if (e.code == 'email-already-in-use') {
-          debugPrint('The account already exists for that email.');
-        } else {
-          debugPrint(e.toString());
-        }
-      } catch (e) {
-        debugPrint(e.toString());
-      }
-    } else {
-      debugPrint('Not Valid');
-    }
+  getData() async {
+    var responseusers = await useref.get();
+
+    responseusers.docs.forEach((element) {
+      setState(() {
+        users.add(element.data());
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(),
-        body: ListView(
-          children: [
-            SizedBox(
-              height: 100,
-            ),
-            Form(
-              key: formState,
-              child: Column(
-                children: [
-                  TextFormField(
-                    onSaved: (newValue) {
-                      username = newValue;
-                    },
-                    validator: (val) {
-                      if (val!.length > 100) {
-                        return 'big user name';
-                      } else if (val.length < 3) {
-                        return 'small user name';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'UserName',
-                    ),
-                  ),
-                  TextFormField(
-                    onSaved: (newValue) {
-                      email = newValue;
-                    },
-                    validator: (val) {
-                      if (val!.length > 100) {
-                        return 'big email';
-                      } else if (val.length < 3) {
-                        return 'small email';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'UserName',
-                    ),
-                  ),
-                  TextFormField(
-                    onSaved: (newValue) {
-                      password = newValue;
-                    },
-                    validator: (val) {
-                      if (val!.length > 100) {
-                        return 'big password';
-                      } else if (val.length < 3) {
-                        return 'small password';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'UserName',
-                    ),
-                  ),
-                  SizedBox(),
-                  GestureDetector(
-                    onTap: () async {
-                      debugPrint("youssef");
-                      await signIn();
-                    },
-                    child: Container(
-                        color: Colors.red,
-                        width: 100,
-                        height: 100,
-                        child: Text('Sign In')),
-                  ),
-                ],
-              ),
-            ),
-          ],
+        body: ListView.builder(
+          itemBuilder: ((context, indexSnapshot) {
+            return Text('${users[indexSnapshot]['name']}');
+          }),
+          itemCount: users.length,
         ),
       ),
     );
